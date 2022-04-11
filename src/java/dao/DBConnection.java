613106d29a9903444e127b290b01bd1f18f41f50
Berkan Acikgoz <berkan_leo@hotmail.com>
2022-04-11 07:06:10 +0300

    entity

---
+++ src/java/dao/DBConnection.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author berka
+ */
+public class DBConnection {
+        
+    public Connection connect(){
+        Connection c = null;
+        
+        try{
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university","postgres","147369");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return c;
+    }
+    
+}
